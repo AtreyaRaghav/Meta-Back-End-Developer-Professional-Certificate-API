1cd4883ca006222f5884e40214645003446813fa
Raghav Atreya <atreyaraghav@gmail.com>
2026-06-02 19:20:28 +0530

    permissions check and models class dunder str method added.

--- LittleLemon/LittleLemonAPI/models.py
+++ LittleLemon/LittleLemonAPI/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=255, db_index=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
+    featured = models.BooleanField(db_index=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"Menu Title {self.title} price {self.price} category {self.category} featured {self.featured}"
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ('menuitem', 'user')
+
+    def __str__(self):
+        return f"user {self.user} menuitem {self.menuitem} quantity {self.quantity} unit_price {self.unit_price} price {self.price}"    
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    delivery_crew = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="delivery_crew", null=True)
+    status = models.BooleanField(db_index=True, default=0)
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+    date =models.DateField(db_index=True)
+
+    def __str__(self):
+        return f"user {self.user} delivery_crew {self.delivery_crew} status {self.status} total {self.total} date {self.date}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(User, on_delete=models.CASCADE)
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ('order', 'menuitem')
+
+    def __str__(self):
+        return f"order {self.order} menuitem {self.menuitem} quantity {self.quantity} unit_price {self.unit_price} price {self.price}"
